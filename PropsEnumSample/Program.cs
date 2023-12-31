3ab36b3b300379e89af4dfefe4819a3cc59cd649
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-12-31 12:39:53 +0900

    remove: comments

--- PropsEnumSample/Program.cs
+++ PropsEnumSample/Program.cs
@@ -1,25 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PropsEnumSample;
-
-//Console.WriteLine($"Free plan is billing enabled: {SubscriptionPlan.Free.IsBillingPromotionFeatureEnabled()}");
-//Console.WriteLine($"Monthly plan is billing enabled: {SubscriptionPlan.Monthly.IsBillingPromotionFeatureEnabled()}");
-//Console.WriteLine($"Yearly plan is billing enabled: {SubscriptionPlan.Yearly.IsBillingPromotionFeatureEnabled()}");
-//Console.WriteLine($"Student plan is billing enabled: {SubscriptionPlan.Student.IsBillingPromotionFeatureEnabled()}");
-
-//Console.WriteLine($"Free plan is content accessible: {SubscriptionPlan.Free.IsLimitedContentAccessible()}");
-//Console.WriteLine($"Monthly plan is content accessible: {SubscriptionPlan.Monthly.IsLimitedContentAccessible()}");
-//Console.WriteLine($"Yearly plan is content accessible: {SubscriptionPlan.Yearly.IsLimitedContentAccessible()}");
-//Console.WriteLine($"Student plan is content accessible: {SubscriptionPlan.Student.IsLimitedContentAccessible()}");
-
-//Console.WriteLine($"Free plan is discount promotion enabled: {SubscriptionPlan.Free.IsDiscountPromotionEnabled()}");
-//Console.WriteLine($"Monthly plan is discount promotion enabled: {SubscriptionPlan.Monthly.IsDiscountPromotionEnabled()}");
-//Console.WriteLine($"Yearly plan is discount promotion enabled: {SubscriptionPlan.Yearly.IsDiscountPromotionEnabled()}");
-//Console.WriteLine($"Student plan is discount promotion enabled: {SubscriptionPlan.Student.IsDiscountPromotionEnabled()}");
-
-//Console.WriteLine($"Free plan is student verification required: {SubscriptionPlan.Free.IsStudentVerificationRequired()}");
-//Console.WriteLine($"Monthly plan is student verification required: {SubscriptionPlan.Monthly.IsStudentVerificationRequired()}");
-//Console.WriteLine($"Yearly plan is student verification required: {SubscriptionPlan.Yearly.IsStudentVerificationRequired()}");
-//Console.WriteLine($"Student plan is student verification required: {SubscriptionPlan.Student.IsStudentVerificationRequired()}");
+﻿using PropsEnumSample;
 
 var subscriptionPlan = SubscriptionPlan.Free;
 
--- PropsEnumSample/SubscriptionPlan.cs
+++ PropsEnumSample/SubscriptionPlan.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PropsEnumSample {
+﻿namespace PropsEnumSample {
     enum SubscriptionPlan {
         [BillingPromotionFeatureEnabled(false)]
         [LimitedContentAccessible(false)]
@@ -29,7 +22,6 @@ namespace PropsEnumSample {
         Student
     }
 
-    // SubscriptionPlanの拡張メソッドを定義する
     static class SubscriptionPlanExtensions {
 
         public static bool IsBillingPromotionFeatureEnabled(this SubscriptionPlan self) {
